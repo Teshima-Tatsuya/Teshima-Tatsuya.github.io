c83a600aab0c939ae0f8aa039d1ac6e2576f8eec
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-05-13 12:59:38 +0900

    Add CssBaseline

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import Header from "./components/Header";
 import Toolbar from "@material-ui/core/Toolbar";
 import Main from "./components/Main";
+import { CssBaseline } from "@material-ui/core";
 
 function App() {
   return (
     <>
+      <CssBaseline />
       <Header />
       <Toolbar />
       <Main />
