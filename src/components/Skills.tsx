6de2e193df978ef40e964a0b0823659cb478371d
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-05-16 11:09:07 +0900

    skills整理中

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,13 +1,86 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import {
+  withStyles,
+  Theme,
+  createStyles,
+  makeStyles,
+} from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+const StyledTableCell = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
+
+const StyledTableRow = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      "&:nth-of-type(odd)": {
+        backgroundColor: theme.palette.action.hover,
+      },
+    },
+  })
+)(TableRow);
+
+const rows = [
+  ["Frozen yoghurt", 159, 6.0, 24, 4.0],
+  ["Ice cream sandwich", 237, 9.0, 37, 4.3],
+  ["Eclair", 262, 16.0, 24, 6.0],
+  ["Cupcake", 305, 3.7, 67, 4.3],
+  ["Gingerbread", 356, 16.0, 49, 3.9],
+];
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+  },
+});
 
 const Skills: React.FC = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <Typography>言語</Typography>
-      <Typography>フレームワーク、ライブラリ</Typography>
-      <Typography>ツール</Typography>
-      <Typography>OS</Typography>
+      <h2>スキル</h2>
+      <pre>整理中です。</pre>
+      <TableContainer component={Paper}>
+        <Table className={classes.table} aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Dessert (100g serving)</StyledTableCell>
+              <StyledTableCell align="right">言語</StyledTableCell>
+              <StyledTableCell align="right">フレームワーク</StyledTableCell>
+              <StyledTableCell align="right">OS</StyledTableCell>
+              <StyledTableCell align="right">ツール</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row[0]}>
+                <StyledTableCell component="th" scope="row">
+                  {row[0]}
+                </StyledTableCell>
+                <StyledTableCell align="right">{row[0]}</StyledTableCell>
+                <StyledTableCell align="right">{row[1]}</StyledTableCell>
+                <StyledTableCell align="right">{row[2]}</StyledTableCell>
+                <StyledTableCell align="right">{row[3]}</StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 };
