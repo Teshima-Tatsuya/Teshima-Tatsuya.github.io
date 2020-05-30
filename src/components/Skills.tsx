0ab09d7df9b797319b4eec5e00ccf6d03510ad23
Tatsuya Teshima <terima666tatsuya@yahoo.co.jp>
2020-05-30 23:17:04 +0900

    skill css fix

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -13,14 +13,61 @@ import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
 
+const StyledTableContainer = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: "80%",
+    },
+  })
+)(TableCell);
+
 const StyledTableCell = withStyles((theme: Theme) =>
   createStyles({
     head: {
-      backgroundColor: theme.palette.common.black,
-      color: theme.palette.common.white,
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.common.black,
     },
     body: {
       fontSize: 14,
+    },
+  })
+)(TableCell);
+
+const CellLang = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.common.black,
+      width: "10%",
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
+
+const CellExpYear = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.common.black,
+      width: "20%",
+    },
+    body: {
+      fontSize: 14,
+    },
+  })
+)(TableCell);
+
+const CellLevel = withStyles((theme: Theme) =>
+  createStyles({
+    head: {
+      backgroundColor: theme.palette.common.white,
+      color: theme.palette.common.black,
+    },
+    body: {
+      fontSize: 14,
+      textAlign: "left",
     },
   })
 )(TableCell);
@@ -49,23 +96,32 @@ const langs = [
 ];
 
 const rows = [
-  ["PHP", 5, 
-  `フレームワーク（FuelPHP,CodeIgniter）を使用し、
+  [
+    "PHP",
+    5,
+    `フレームワーク（FuelPHP,CodeIgniter）を使用し、
   キュレーションサービスのバックエンドを作成可能。また、
-  PurePHPを用いてフルスクラッチでSNSを作成した経験あり`
-　],
-  ["Ruby", 3, 
-  `フレームワーク（Ruby on Rails）を使用しキュレーションサービスのバックエンドを作成可能。
-  また、Webスクレイピングによる情報収集等も可能`
+  PurePHPを用いてフルスクラッチでSNSを作成した経験あり`,
   ],
-  ["JavaScript", 1, 
-  `React, jQueryを用いた簡易なWebサイト構築可能。
+  [
+    "Ruby",
+    3,
+    `フレームワーク（Ruby on Rails）を使用しキュレーションサービスのバックエンドを作成可能。
+  また、Webスクレイピングによる情報収集等も可能`,
+  ],
+  [
+    "JavaScript",
+    1,
+    `React, jQueryを用いた簡易なWebサイト構築可能。
   また、Ruby on Railsをバックエンドとして開発も経験
-  `],
-  ["VBA", 3,
-  `Seleniumが使用不可能な環境で、社内のWebテスト自動化ツールを作成
+  `,
+  ],
+  [
+    "VBA",
+    3,
+    `Seleniumが使用不可能な環境で、社内のWebテスト自動化ツールを作成
   社内の誰もが使用可能となるよう、Excelでミニ言語を作成し、簡易にシナリオ作成を可能とした。
-  また、社内向けの定常作業の自動化も多く実装し、全体の工数削減に貢献`
+  また、社内向けの定常作業の自動化も多く実装し、全体の工数削減に貢献`,
   ],
 ];
 const useStyles = makeStyles({
@@ -80,27 +136,26 @@ const Skills: React.FC = () => {
   return (
     <>
       <h2>スキル</h2>
-      <pre>整理中です。</pre>
-      <TableContainer component={Paper}>
+      <StyledTableContainer component={Paper}>
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
             <TableRow>
-              <StyledTableCell align="right">言語</StyledTableCell>
-              <StyledTableCell align="right">経験年数</StyledTableCell>
-              <StyledTableCell align="right">レベル</StyledTableCell>
+              <CellLang align="right">言語</CellLang>
+              <CellExpYear align="right">経験年数</CellExpYear>
+              <CellLevel align="right">レベル</CellLevel>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
               <StyledTableRow key={row[0]}>
-                <StyledTableCell align="right">{row[0]}</StyledTableCell>
-                <StyledTableCell align="right">{row[1]}</StyledTableCell>
-                <StyledTableCell align="right">{row[2]}</StyledTableCell>
+                <CellLang align="right">{row[0]}</CellLang>
+                <CellExpYear align="right">{row[1]}</CellExpYear>
+                <CellLevel align="right">{row[2]}</CellLevel>
               </StyledTableRow>
             ))}
           </TableBody>
         </Table>
-      </TableContainer>
+      </StyledTableContainer>
     </>
   );
 };
